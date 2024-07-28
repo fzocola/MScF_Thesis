656f10ba36dc6fc6c36e8bf446222072a21127c6
fzocola <flo.zocola@gmail.com>
2024-07-28 17:10:12 +0200

    DATA MANAGEMENT

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Import packages
 from pathlib import Path
 import pandas as pd
+from datetime import datetime, timedelta
+import numpy as np
 
 # Project directories paths
 paths = {'main': Path.cwd()}
@@ -16,36 +18,88 @@ paths.update({'scripts': Path.joinpath(paths.get('main'), 'scripts')})
 
 dic_fm_data = pd.read_excel(Path.joinpath(paths.get('data'), 'Thesis_Data_Importable.xlsx'), sheet_name=None)
 
-
+'''
 for name, df in dic_fm_data.items():
     print(name)
+'''
 
 ls_fundamental_sheet_name = ['LT_DEBT', 'ST_DEBT', 'CFO', 'INTEREST_EXP', 'EBITDA', 'NET_DEBT', 'TOT_ASSET',
-                             'WORK_CAP','RET_EARN', 'EBIT', 'SALES', 'CUR_ASSET', 'CUR_LIAB', 'BOOK_EQUITY',
+                             'WORK_CAP', 'RET_EARN', 'EBIT', 'SALES', 'CUR_ASSET', 'CUR_LIAB', 'BOOK_EQUITY',
                              'CASH', 'NET_INCOME']
-
-dic_issuer_fundamental_quarterly =
+dic_issuer_fundamental_quarterly = {i: dic_fm_data[i] for i in ls_fundamental_sheet_name}
 
 ls_issuer_market_sheet_name = ['RATING', 'MKT_CAP', 'TOT_RETURN', 'SHARE_PRICE', 'CDS_SPREAD_5Y']
-
-dic_issuer_market_daily =
+dic_issuer_market_daily = {i: dic_fm_data[i] for i in ls_issuer_market_sheet_name}
 
 ls_market_sheet_name = ['RATES', 'SPX']
-
-dic_market_data_daily =
+dic_market_data_daily = {i: dic_fm_data[i] for i in ls_market_sheet_name}
 
 ls_issuer_description_sheet_name = ['ISSUER', 'CDS_TICKER']
+dic_issuer_description = {i: dic_fm_data[i] for i in ls_issuer_description_sheet_name}
 
-dic_issuer_description =
 
-(ISSUER,LT_DEBT,ST_DEBT, CFO, INTEREST_EXP, EBITDA, NET_DEBT, TOT_ASSET, WORK_CAP, RET_EARN, EBIT, SALES, CUR_ASSET,
- CUR_LIAB, BOOK_EQUITY, CASH, NET_INCOME)
-RATING
-MKT_CAP
-TOT_RETURN
-SHARE_PRICE
-CDS_TICKER
-CDS_SPREAD_5Y
-RATES
-SPX
+def get_market_open_date(s_ref_data):
+    s_ref_data = s_ref_data.dropna()
+    s_market_open_date = pd.Series(s_ref_data.index)
+    return s_market_open_date
 
+
+s_market_open_date = get_market_open_date(dic_market_data_daily['SPX'].set_index('DATES')['px_last'])
+
+
+def set_index_df_in_dic(dic, index):
+    for i in dic:
+        dic[i] = dic[i].set_index(index)
+    return dic
+
+
+dic_issuer_fundamental_quarterly = set_index_df_in_dic(dic_issuer_fundamental_quarterly, 'DATES')
+
+
+def df_conv_quarterly_to_daily(df):
+    na_to_nb_value = -9999999999999
+    df = df.fillna(na_to_nb_value)
+
+    # Create a date range that includes all daily dates within the range of the quarterly data
+    daily_index = pd.date_range(start=df.index.min(), end=df.index.max() + timedelta(days=60), freq='D')
+
+    # Reindex the DataFrame to the daily date range and forward-fill the values
+    df_daily = df.reindex(daily_index).ffill()
+    df_daily = df_daily.replace(na_to_nb_value, np.nan)
+
+    return df_daily
+
+def dic_conv_quarterly_to_daily(dic):
+    for i in dic:
+        dic[i] = df_conv_quarterly_to_daily(dic[i])
+    return dic
+
+
+dic_issuer_fundamental_quarterly = dic_conv_quarterly_to_daily(dic_issuer_fundamental_quarterly)
+
+df_test_1 = dic_issuer_fundamental_quarterly['LT_DEBT']
+
+
+'''
+df_test = dic_market_data_daily['SPX']['px_last']
+s_spx_price = dic_market_data_daily['SPX'].set_index('DATES')['px_last'].dropna()
+s_market_open_date = pd.Series(s_spx_price.index)
+'''
+
+'''
+df_test = dic_issuer_fundamental_quarterly['LT_DEBT']
+na_to_nb_value = -9999999999999
+df_test = dic_issuer_fundamental_quarterly['LT_DEBT'].fillna(na_to_nb_value)
+
+# Create a date range that includes all daily dates within the range of the quarterly data
+daily_index = pd.date_range(start=df_test.index.min(), end=df_test.index.max() + timedelta(days=60), freq='D')
+
+# Reindex the DataFrame to the daily date range and forward-fill the values
+df_daily = df_test.reindex(daily_index).ffill()
+df_daily = df_daily.replace(na_to_nb_value, np.nan)
+'''
+
+'''
+df_test_1 = dic_issuer_fundamental_quarterly['LT_DEBT']
+df_test_2 = df_conv_quarterly_to_daily(dic_issuer_fundamental_quarterly['LT_DEBT'])
+'''
